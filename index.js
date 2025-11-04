8a711297d0d917c2dfd164f51a414b0208504c41
Gowrav <gowravvenkatesh4@gmail.com>
2025-11-04 16:04:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get("/:name", (req, res) => {
 });
 
 app.get("/", (req, res) => {
-  res.send(`baby u light up my world like nobuddy else the way that u flip ur hair gets me overwhemeled ..but when u smile at the ground aint it hard to tell u dont know ohh ohh `);
+  res.send(`wanna br your , do i wanna know ,lover ,blank sapce,nothing holding me back`);
 });
 
 app.use((req, res, next) => {
